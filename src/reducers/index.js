733b40e09e5868c1d60b4bf04eab279be86c4f52
Connor Hansen <chansen13@georgefox.edu>
2021-12-07 13:40:36 -0800

    WIP firebase issues

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
-import 'firebase/auth';
+import 'firebase/compat/auth';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
